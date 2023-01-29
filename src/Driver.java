5a8c92bf6356dd8a04193f9f37b7c377b0b0e3e7
Andrew <andrewalson1@gmail.com>
2023-01-28 19:38:10 -0500

    First commit. Driver class.

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+import com.sun.tools.javac.Main;
+import view.MainView;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        new MainView();
+
+    }
+}
---
+++ src/view/MainView.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.*;
+
+public class MainView extends JFrame {
+
+    public MainView() {
+        super();
+        this.setSize(1500, 1000);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
